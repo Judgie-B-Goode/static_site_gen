73afb303c804b1d074af2486a3d4569702b31e4c
Judgie <judgie375@gmail.com>
2025-01-13 10:42:58 -0500

    PRE6: text node to html node

--- src/textnode.py
+++ src/textnode.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from htmlnode import LeafNode
 
 class TextType(Enum):
 	NORMAL = "normal"
@@ -21,3 +22,18 @@ class TextNode():
 
 	def __repr__(self):
 		return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+
+	def text_node_to_html_node(self, text_node):
+		if self.text_type == TextType.NORMAL:
+			return LeafNode(None, self.text, props=None)
+		if self.text_type == TextType.BOLD:
+			return LeafNode("b", self.text, props=None)
+		if self.text_type == TextType.ITALIC:
+			return LeafNode("i", self.text, props=None)
+		if self.text_type == TextType.CODE:
+			return LeafNode("code", self.text, props=None)
+		if self.text_type == TextType.LINKS:
+			return LeafNode("a", self.text, props="href=")
+		if self.text_type == TextType.IMAGES:
+			return LeafNode("img", "", props={"src":text_node.url,"alt" : text_node.text})
+		raise Exception("Invalid Type")
