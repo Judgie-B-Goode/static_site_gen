ffa22c16459f9042d74b199f0549cc87b671ac9e
Judgie <judgie375@gmail.com>
2025-01-13 08:44:41 -0500

    PRE5: retooled to align better with the course work as I had wildly deviated

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -1,19 +1,19 @@
 class HTMLNode():
-    def __init__(self, tag="None", value="None", children="None", props="None"):
+    def __init__(self, tag=None, value=None, children=None, props=None):
         self.tag = tag  #HTML Tag
         self.value = value #Text inside the tag
         self.children = children #Children of this node
         self.props = props #Attributes of this tag
 
     def to_html(self):
-        raise NotImplementedError
+        raise NotImplementedError("Not Implemented")
     
     def props_to_html(self):
         props_string = ""
-        if self.props == "None":
-            return self.props
-        for key,value in self.props.items():
-            props_string = props_string + str(key) + '=' + "\"" + str(value) + "\" "
+        if self.props is None:
+            return ""
+        for prop in self.props:
+            props_string += f' {prop}="{self.props[prop]}"'
         return props_string
     
     def __repr__(self):
@@ -21,46 +21,32 @@ class HTMLNode():
     
         
 class LeafNode(HTMLNode):
-    def __init__(self, tag="None", value="None", props="None"):
-        super().__init__()
-        self.tag = tag  #HTML Tag
-        self.value = value #Text inside the tag
-        self.props = props #Attributes of this tag
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
 
     def to_html(self):
-        local_props = ""
-        if self.value == "None":
+        if self.value is None:
             raise ValueError("Missing Value")
-        if self.tag == "None":
+        if self.tag is None:
             return str(self.value)
-        if self.props == "None":
-            local_props = ""
-            return f"<{self.tag}{local_props}>{self.value}</{self.tag}>"
-        else:
-            for key,value in self.props.items():
-                local_props = local_props + str(key) + '=' + "\"" + str(value) + "\""
-                return f"<{self.tag} {local_props}>{self.value}</{self.tag}>"
+        
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
             
 
 
 class ParentNode(HTMLNode):
-    def __init__(self, tag="None", value="None", children="None", props="None"):
-        super().__init__()
-        self.tag = tag  #HTML Tag
-        self.value = value #Text inside the tag
-        self.children = children #Children of this node
-        self.props = props #Attributes of this tag
+    def __init__(self, tag, children, props=None):
+        super().__init__(tag, None, children, props)
 
-    def child_node_digger(self, children):
-        if len(children) == 0:
-            return ""
-        else:
-            return str(children[0].to_html) + self.child_node_digger(children[1:])
 
     def to_html(self):
         child_nodes = ""
-        if self.tag == "None":
+        if self.tag is None:
             raise ValueError("Tag is required")
-        if self.children == "None":
+        if self.children is None:
             raise ValueError("Parents have children")
-        return self.child_node_digger(self.children)
+        for child in self.children:
+            child_nodes += child.to_html()
+        return f"<{self.tag}{self.props_to_html()}>{child_nodes}</{self.tag}>"
+    def __repr__(self):
+        return f"ParentNode({self.tag}, children: {self.children}, {self.props})"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,31 +1,72 @@
 import unittest
-from htmlnode import HTMLNode
-from htmlnode import LeafNode
+from htmlnode import HTMLNode, LeafNode, ParentNode
 
+class TestHTMLNode(unittest.TestCase):
+    def test_props1(self):
+        tester = HTMLNode(tag="<a>",value="Some Stuff")
+        print(tester.props_to_html()) 
 
+    def test_props2(self):
+        tester = HTMLNode(tag="<a>",value="Some Stuff", props= {"href": "https://www.google.com", 
+        "target": "_blank"})
+        print(tester.props_to_html())
 
-def test_props1():
-    tester = HTMLNode(tag="<a>",value="Some Stuff")
-    print(tester.props_to_html()) 
+    def test_props3(self):
+        tester = HTMLNode(tag="<a>",value="Some Stuff", props= {"href": "https://www.google.com", 
+        "target": "_blank", "height" : 500, "width" : 500})
+        print(tester.props_to_html())
 
-def test_props2():
-    tester = HTMLNode(tag="<a>",value="Some Stuff", props= {"href": "https://www.google.com", 
-    "target": "_blank"})
-    print(tester.props_to_html())
+    def test_leaf1(self):
+        leaftest = LeafNode(tag = "a", value = "POOP", props={"butt":"junk"})
+        print(leaftest.to_html())
 
-def test_props3():
-    tester = HTMLNode(tag="<a>",value="Some Stuff", props= {"href": "https://www.google.com", 
-    "target": "_blank", "height" : 500, "width" : 500})
-    print(tester.props_to_html())
+    def test_leaf3(self):
+        leaftest = LeafNode(None, value = "POOP", props={"butt":"junk"})
+        print(leaftest.to_html())
 
-def test_leaf1():
-    leaftest = LeafNode(tag = "a", value = "POOP", props={"butt":"junk"})
-    print(leaftest.to_html())
+    def test_to_html_with_children(self):
+            child_node = LeafNode("span", "child")
+            parent_node = ParentNode("div", [child_node])
+            self.assertEqual(parent_node.to_html(), "<div><span>child</span></div>")
 
-def test_leaf2():
-    leaftest = LeafNode(tag = "a", props={"butt":"junk"})
-    print(leaftest.to_html())
+    def test_to_html_with_grandchildren(self):
+        grandchild_node = LeafNode("b", "grandchild")
+        child_node = ParentNode("span", [grandchild_node])
+        parent_node = ParentNode("div", [child_node])
+        self.assertEqual(
+            parent_node.to_html(),
+            "<div><span><b>grandchild</b></span></div>",
+        )
 
-def test_leaf3():
-    leaftest = LeafNode(value = "POOP", props={"butt":"junk"})
-    print(leaftest.to_html())
+    def test_to_html_many_children(self):
+        node = ParentNode(
+            "p",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p>",
+        )
+
+    def test_headings(self):
+        node = ParentNode(
+            "h2",
+            [
+                LeafNode("b", "Bold text"),
+                LeafNode(None, "Normal text"),
+                LeafNode("i", "italic text"),
+                LeafNode(None, "Normal text"),
+            ],
+        )
+        self.assertEqual(
+            node.to_html(),
+            "<h2><b>Bold text</b>Normal text<i>italic text</i>Normal text</h2>",
+        )
+        
+if __name__ == "__main__":
+    unittest.main()
